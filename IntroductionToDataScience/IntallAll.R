9d2b724e7d555b283faf268ce95405923ecb79c4
John Mount <jmount@win-vector.com>
2015-12-11 10:29:57 -0800

    add intall instructions

---
+++ IntroductionToDataScience/IntallAll.R
@@ -0,0 +1,4 @@
+install.packages(c('GGally', 'RJDBC', 'ROCR', 'RSQLite', 'devtools',
+  'gbm', 'gdata', 'ggplot2', 'glmnet', 'hexbin', 'kernlab', 'mgcv',
+  'mvtnorm', 'plyr', 'quantmod', 'quantreg', 'randomForest', 'reshape2',
+  'rpart', 'sqldf', 'stringr', 'vtreat'))
