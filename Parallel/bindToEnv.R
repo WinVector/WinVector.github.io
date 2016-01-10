48765444518b4819f625fcccd32bfd38b3ccb1b0
John Mount <jmount@win-vector.com>
2016-01-10 10:31:46 -0800

    edit

---
+++ Parallel/bindToEnv.R
@@ -0,0 +1,27 @@
+#' Copy arguments into env and re-bind any function's lexical scope to bindTargetEnv .
+#' 
+#' See http://winvector.github.io/Parallel/PExample.html for example use.
+#' 
+#' 
+#' Used to send data along with a function in situations such as parallel execution 
+#' (when the global environment would not be available).  Typically called within 
+#' a function that constructs the worker function to pass to the parallel processes
+#' (so we have a nice lexical closure to work with).
+#' 
+#' @param bindTargetEnv environment to bind to
+#' @param objNames additional names to lookup in parent environment and bind
+#' @param names of functions to NOT rebind the lexical environments of
+bindToEnv <- function(bindTargetEnv=parent.frame(),objNames,doNotRebind=c()) {
+  # Bind the values into environment
+  # and switch any functions to this environment!
+  for(var in objNames) {
+    val <- get(var,envir=parent.frame())
+    if(is.function(val) && (!(var %in% doNotRebind))) {
+      # replace function's lexical environment with our target (DANGEROUS)
+      environment(val) <- bindTargetEnv
+    }
+    # assign object to target environment, only after any possible alteration
+    assign(var,val,envir=bindTargetEnv)
+  }
+}
+
--- Parallel/bindToEnv2.R
+++ Parallel/bindToEnv2.R
@@ -1,5 +1,8 @@
 #' Copy arguments into env and re-bind any function's lexical scope to bindTargetEnv .
 #' 
+#' See http://winvector.github.io/Parallel/PExample.html for example use.
+#' 
+#' 
 #' Used to send data along with a function in situations such as parallel execution 
 #' (when the global environment would not be available).  Typically called within 
 #' a function that constructs the worker function to pass to the parallel processes
@@ -10,7 +13,7 @@
 #' @param objNames additional names to lookup in parent environment and bind
 #' @param names of functions to NOT rebind the lexical environments of
 bindToEnv <- function(...,bindTargetEnv=parent.frame(),objNames=c(),doNotRebind=c()) {
-  # get names of variables used as ... aguments in function call
+  # get names of variables used as ... arguments in function call
   dnames <- sapply(substitute(list(...))[-1],deparse)
   # get the values
   dvalues <- list(...)
@@ -18,14 +21,14 @@ bindToEnv <- function(...,bindTargetEnv=parent.frame(),objNames=c(),doNotRebind=
   if(!is.null(names(dvalues))) {
     stop("unexpected named assignments")
   }
-  # merge in any value (non "=") assigments as names
+  # merge in any value (non "=") assignments as names
   names(dvalues) <- dnames
   # now bind the values into environment
   # and switch any functions to this environment!
   for(var in names(dvalues)) {
     val <- dvalues[[var]]
     if(is.function(val) && (!(var %in% doNotRebind))) {
-      # replace function's lexical environemnt with our target (DANGEROUS)
+      # replace function's lexical environment with our target (DANGEROUS)
       environment(val) <- bindTargetEnv
     }
     # assign object to target environment, only after any possible alteration
@@ -36,7 +39,7 @@ bindToEnv <- function(...,bindTargetEnv=parent.frame(),objNames=c(),doNotRebind=
     for(var in objNames) {
       val <- get(var,envir=parent.frame())
       if(is.function(val) && (!(var %in% doNotRebind))) {
-        # replace function's lexical environemnt with our target (DANGEROUS)
+        # replace function's lexical environment with our target (DANGEROUS)
         environment(val) <- bindTargetEnv
       }
       # assign object to target environment, only after any possible alteration
