a643012f151c255f05170a710d604b42f9c5e553
John Mount <jmount@win-vector.com>
2017-08-28 17:48:39 -0700

    re-run with xgboost

---
+++ KDD2009/xgboost.R
@@ -0,0 +1,68 @@
+
+library("xgboost")
+library("tidyr")
+
+
+# outcome should be 0/1 numeric
+mkXGBoostModel <- function(trainDat, vars, outcomeCol, 
+                           nTrain= 200000,
+                           xval= TRUE,
+                           ntrees= 1000,
+                           eta = 0.1,
+                           depth = 8,
+                           nfold = 5) {
+  # shuffle and control size
+  print(paste("start mkXGBoostModel", base::date()))
+  seed <- sample.int(1000000000,1)
+  nthreads <- parallel::detectCores()
+  trainDat <- trainDat[, c(vars, outcomeCol), drop=FALSE]
+  complete <- complete.cases(trainDat)
+  trainDat <- trainDat[complete, , drop=FALSE]
+  trainDat <- trainDat[sample.int(nrow(trainDat), 
+                                  size=min(nrow(trainDat),nTrain),
+                                  replace=FALSE), , 
+                       drop=FALSE]
+  if(xval) {
+    print(paste(" start mkXGBoostModel xgb.cv", base::date()))
+    cv <- xgb.cv(data = as.matrix(trainDat[, vars, drop=FALSE]),
+                 label =  ifelse(trainDat[[outcomeCol]]>0, 1, 0),
+                 nrounds = ntrees,
+                 nfold = nfold,
+                 objectitve = "binary:logistic",
+                 eta = eta,
+                 depth = depth,
+                 verbose = 0,
+                 seed = seed,
+                 nthreads = nthreads)
+    # Get the evaluation log and call summary on it
+    elog <- cv$evaluation_log
+    plotD <- gather(elog, key=errtype, value=rmse, train_rmse_mean, test_rmse_mean) 
+    print(ggplot(plotD, aes(x=iter, y=rmse, color=errtype)) + geom_point() + geom_line() +
+            scale_color_brewer(palette="Dark2"))
+    ntrees <- which.min(elog$test_rmse_mean)
+    cv <- NULL
+    elog <- NULL
+  }
+  print(paste("xgboost: ntrees", ntrees))
+  print(paste(" start mkXGBoostModel xgboost", base::date()))
+  model <- xgboost(data = as.matrix(trainDat[, vars, drop=FALSE]),
+                   label =  ifelse(trainDat[[outcomeCol]]>0, 1, 0),
+                   nrounds = ntrees,
+                   objectitve = "binary:logistic",
+                   eta = eta,
+                   depth = depth,
+                   verbose = 0,
+                   seed = seed,
+                   nthreads = nthreads)
+  # clear vars to prevent dangling refs
+  trainDat <- NULL
+  print(paste("done mkXGBoostModel", base::date()))
+  # return prediction function
+  function(dat) {
+    preds <- predict(model,
+                     newdata =  as.matrix(dat[, vars, drop=FALSE]), 
+                     ntreelimit = ntrees)
+    preds
+  }
+}
+
